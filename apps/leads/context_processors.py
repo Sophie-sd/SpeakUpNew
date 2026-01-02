ecbfb1c653e24e4261462dcdd03a45dc42981eeb
BonisOleg <oleg@coresync.life>
2026-01-02 20:22:04 +0200

    ++

--- apps/leads/context_processors.py
+++ apps/leads/context_processors.py
@@ -2,14 +2,21 @@ from .models import RunningLineText
 
 
 def running_line_context(request):
-    """Додає текст бігучої стрічки у всі templates"""
+    """Додає тексти бігучої стрічки у всі templates"""
     try:
-        running_line = RunningLineText.objects.filter(is_active=True).first()
+        running_lines = RunningLineText.objects.filter(is_active=True).order_by('order', 'id')
+        running_line_texts = [line.text for line in running_lines]
+        # Для сумісності зі старим кодом, якщо потрібен один текст
+        running_line_text = running_line_texts[0] if running_line_texts else ''
         return {
-            'running_line_text': running_line.text if running_line else ''
+            'running_line_texts': running_line_texts,
+            'running_line_text': running_line_text  # Для сумісності
         }
     except:
-        return {'running_line_text': ''}
+        return {
+            'running_line_texts': [],
+            'running_line_text': ''
+        }
 
 
 
